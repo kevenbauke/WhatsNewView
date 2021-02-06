a168cf22779181a920a4ba59905239ba5c5eb9c2
Keven Bauke <mail@kevenbauke.com>
2021-02-06 15:11:38 +0100

    Add public init.

--- Sources/WhatsNewView/WhatsNewView.swift
+++ Sources/WhatsNewView/WhatsNewView.swift
@@ -2,6 +2,10 @@ import SwiftUI
 
 public struct WhatsNewView: View {
 	@available(iOS 13.0.0, *)
+
+	public init() {}
+
+	@available(iOS 13.0.0, *)
 	public var body: some View {
 		Text("Hello, world!")
 			.padding()
