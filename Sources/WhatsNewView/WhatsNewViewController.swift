8e7865803f67085cd920fc0a1751cf2eb84ae703
Keven Bauke <mail@kevenbauke.com>
2021-03-06 17:36:46 +0100

    Add more initialisers to the controller.

--- Sources/WhatsNewView/WhatsNewViewController.swift
+++ Sources/WhatsNewView/WhatsNewViewController.swift
@@ -2,8 +2,29 @@ import SwiftUI
 
 @available(iOS 13.0, *)
 public class WhatsNewViewController: UIHostingController<WhatsNewView> {
+	public init?(versionPlistPath: String) throws {
+		do {
+			guard let whatsNewView = try WhatsNewView(versionPlistPath: versionPlistPath) else { return nil
+			}
+			super.init(rootView: whatsNewView)
+		} catch {
+			throw error
+		}
+	}
+
 	public init(configuration: WhatsNewConfiguration) {
 		super.init(rootView: WhatsNewView(configuration: configuration))
+	}
+
+	public init?(configurationPlistPath: String) throws {
+		do {
+			guard let whatsNewView = try WhatsNewView(configurationPlistPath: configurationPlistPath) else {
+				return nil
+			}
+			super.init(rootView: whatsNewView)
+		} catch {
+			throw error
+		}
 	}
 
 	@objc required dynamic init?(coder aDecoder: NSCoder) {
